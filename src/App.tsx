7f936373ae9b76880ab704f313254e96aa3b84b6
Henry Zhang <hello@zhanghe.dev>
2024-08-02 18:14:08 +0800

    feat(configs-build-and-lint): enable bundler mode for tsconfig.app.json
    Enable bundler mode by setting "moduleResolution" to "bundler" and allow importing TS extensions in the tsconfig.app.json file.
    Configure paths in tsconfig.app.json to point to src/* under @/* alias.
    Configure paths in tsconfig.node.json to point to src/* under @/* alias.
    Update tsconfig.app.json to include the "baseUrl": "." and "paths" configuration.
    Update tsconfig.node.json to include the "compilerOptions": { "baseUrl": ".", "paths": { "@/*": ["./src/*"] } }.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import './App.css'
+import { pipeline } from "@xenova/transformers";
+import { useCallback, useState } from "react";
+import { Textarea } from "./components/ui/textarea";
+import { Button } from "./components/ui/button";
 
+// env.useBrowserCache = false;
+// env.allowLocalModels = false;
 function App() {
+  const [result, setResult] = useState<unknown>(null);
+  const [value, setValue] = useState('');
+  const handleClick = useCallback(async () => {
+    const classifier = await pipeline('sentiment-analysis');
+    const res = await classifier(value);
+    console.log(result);
+    setResult(res);
+  }, [result, value]);
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="p-4 container mx-auto">
+      <div className="grid w-full gap-2">
+        <Textarea value={value} onChange={({ target: { value: v } }) => setValue(v)} placeholder="Type your message here." />
+        <Button onClick={handleClick}>Test</Button>
+      </div>
+      {!!result && <p>{JSON.stringify(result)}</p>}
+    </div>
   )
 }
 
